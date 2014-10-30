9a2c70d3b9f95428af314b794ff35dada80e40a3
Jan Krems <jan.krems@groupon.com>
2014-10-29 22:08:38 -0700

    Simpler example

--- example/app.js
+++ example/app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var http = require('http');
+var fs = require('fs');
 
 var express = require('express');
 var controller = require('quinn-controller');
@@ -22,17 +23,25 @@ app.use(injectMiddleware(inject, 'request'));
 
 var PostsController = {
   show: inject.action(function(params, query, I18n) {
-    return respond.json({
-      id: params.postId,
-      page: parseInt(query.page, 10),
-      title: I18n.translate('.hello')
-    }, null, 2);
+    return respond()
+      .json({
+        id: params.postId,
+        page: parseInt(query.page, 10),
+        title: I18n.translate('.hello')
+      }, null, 2);
   })
 };
 
 // ROUTING
 
 var action = _.compose(toExpress, controller('posts', PostsController));
+
+app.get('/simple', toExpress(function(req, params) {
+  return respond()
+    .html(fs.createReadStream(__dirname + '/simple.html'))
+    .header('x-fancy', '1000')
+    .status(201);
+}));
 
 app.get('/:postId', action('show'));
 
@@ -41,7 +50,7 @@ app.get('/:postId', action('show'));
 var server = http.createServer(app);
 
 server.listen(process.env.PORT || 3000, function() {
-  var baseUrl = 'http://127.0.0.1:' + this.address().port + '/xyz?page=2';
+  var baseUrl = 'http://127.0.0.1:' + this.address().port + '/simple';
   http.get(baseUrl, function(res) {
     console.log(res.statusCode, res.headers);
     res.pipe(process.stdout, { end: false });
