b3e6d990965cd7c2fa76979605480f792d450428
Jan Krems <jan.krems@groupon.com>
2014-11-03 19:58:38 -0800

    Lower impact, rough test

--- test/inject.js
+++ test/inject.js
@@ -24,5 +24,38 @@ describe('inject', function() {
         })
         .nodeify(done);
     });
+
+    describe('with "local" scope', function() {
+      var _tracker = {};
+
+      before(function() {
+        inject.getScope('local')
+          .register('l1', function(x, y, two) { return x + y + two; })
+          .register('tracker', function($args) { return $args[0]; })
+          .register('two', function($args) { return $args[1]; })
+          .register('y', function() { return 25; });
+      });
+
+      it('shadows global objects', function(done) {
+        inject.runInScope('local', [_tracker, 2], function() {
+          var getStuff = inject(function(l1, tracker) {
+            return [ l1, tracker ];
+          });
+
+          return getStuff(_tracker)
+            .spread(function(l1, tracker) {
+              assert.equal(tracker, _tracker);
+              assert.equal(l1, 3 + 25 + 2);
+            });
+        }).nodeify(done);
+      });
+
+      it('does not pollute the tracker object too much', function(done) {
+        inject.runInScope('locale', [_tracker], function() {
+          assert.equal('{}', JSON.stringify(_tracker));
+          assert.deepEqual({}, _tracker);
+        }).nodeify(done);
+      });
+    });
   });
 });
