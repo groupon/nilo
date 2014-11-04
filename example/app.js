cfbafbe83981bfe207cef514594f99b6fc73cacf
Jan Krems <jan.krems@groupon.com>
2014-11-03 20:33:54 -0800

    An example unit-y test for a response

--- example/app.js
+++ example/app.js
@@ -8,6 +8,8 @@ var controller = require('quinn-controller');
 var respond = require('quinn-respond');
 var toExpress = require('quinn-express');
 var _ = require('lodash');
+var assert = require('assertive');
+var Bluebird = require('bluebird');
 
 var injectMiddleware = require('../middleware');
 
@@ -24,6 +26,7 @@ app.use(injectMiddleware(inject, 'request'));
 var PostsController = {
   show: inject.action(function(params, query, I18n) {
     return respond()
+      .status(400)
       .json({
         id: params.postId,
         page: parseInt(query.page, 10),
@@ -31,6 +34,24 @@ var PostsController = {
       }, null, 2);
   })
 };
+
+// TESTING
+
+var TestI18n = { translate: function(key) { return key; } };
+Bluebird.try(PostsController.show.fn, [
+    { postId: 'x' }, { page: '9' }, TestI18n
+  ])
+  .then(function(testResponse) {
+    assert.equal(400, testResponse.statusCode);
+    assert.equal(
+      'application/json; charset=utf-8',
+      testResponse.getHeader('content-type'));
+    assert.deepEqual({
+      id: 'x', page: 9, title: '.hello'
+    }, testResponse.getData());
+    console.log('Unit tested the response.');
+  })
+  .done();
 
 // ROUTING
 
@@ -52,9 +73,16 @@ var server = http.createServer(app);
 server.listen(process.env.PORT || 3000, function() {
   var baseUrl = 'http://127.0.0.1:' + this.address().port + '/simple';
   http.get(baseUrl, function(res) {
-    console.log(res.statusCode, res.headers);
-    res.pipe(process.stdout, { end: false });
+    assert.equal(201, res.statusCode);
+    assert.equal('1000', res.headers['x-fancy']);
+    assert.equal('text/html; charset=utf-8', res.headers['content-type']);
+    res.setEncoding('utf8');
+    var buffer = '';
+    res.on('data', function(chunk) { buffer += chunk; });
     res.on('end', function() {
+      assert.include('<!DOCTYPE html>', buffer);
+      assert.include('</html>\n', buffer);
+      console.log('Integration tested the response.');
       server.close();
     });
   });
