aebda7f6bbbc6c0c6706ceca861a94435d7819e4
Jan Krems <jan.krems@groupon.com>
2014-11-03 21:07:11 -0800

    Slightly nicer scope setup syntax

--- README.md
+++ README.md
@@ -3,8 +3,29 @@
 A pretty stupid DI framework that just hides itself using random properties
 on an object that needs to be passed around explicitly.
 
-Example with express + quinn:
+Example with express + quinn + magic:
 
-```
+```js
+var express = require('express');
+var toExpress = require('quinn-express');
+var respond = require('quinn-respond');
+var inject = require('quinn-inject')();
 
+inject.getRootScope()
+  .register('x', function() { return 10; });
+
+inject.getScope('action')
+  .setArguments([ 'req', 'params' ])
+  .register('query', function(req) { return req.query; });
+
+// Create express app
+var app = express();
+
+function *myHandler(params, x, query) {
+  var obj = loadFromDatabase(params.id);
+  return respond()
+    .json({ obj: yield obj, x: x, query: query });
+}
+
+app.get('/my/:id', toExpress(inject.action(myHandler)));
 ```
--- example/env.js
+++ example/env.js
@@ -4,36 +4,34 @@ var createEnvironment = require('../lib/inject');
 
 var inject = createEnvironment();
 
-var requestScope = inject.getScope('request');
-requestScope.setArguments([ 'req', 'res' ]);
+inject.getScope('request')
+  .setArguments([ 'req', 'res' ])
+  .register('query', function(req) {
+    return req.query;
+  });
 
-requestScope.register('query', function(req) {
-  return req.query;
-});
-
-var actionScope = inject.getScope('action');
-actionScope.setArguments([ 'req', 'params' ]);
-
-actionScope.register('I18n', function(params) {
-  return {
-    locale: 'en_US',
-    lang: 'en',
-    scope: [ params.controllerName ],
-    data: {
-      posts: {
-        hello: 'Hello World'
+inject.getScope('action')
+  .setArguments([ 'req', 'params' ])
+  .register('I18n', function(params) {
+    return {
+      locale: 'en_US',
+      lang: 'en',
+      scope: [ params.controllerName ],
+      data: {
+        posts: {
+          hello: 'Hello World'
+        }
+      },
+      translate: function(key) {
+        var keyPath = key.split('.');
+        if (keyPath[0] === '') {
+          keyPath = this.scope.concat(keyPath.slice(1));
+        }
+        return keyPath.reduce(function(node, prop) {
+          return node && node[prop];
+        }, this.data);
       }
-    },
-    translate: function(key) {
-      var keyPath = key.split('.');
-      if (keyPath[0] === '') {
-        keyPath = this.scope.concat(keyPath.slice(1));
-      }
-      return keyPath.reduce(function(node, prop) {
-        return node && node[prop];
-      }, this.data);
     }
-  }
-});
+  });
 
 module.exports = inject;
